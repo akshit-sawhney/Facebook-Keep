3482b99145f046f512c1733777cdd13c71e39a22
akshitbrightlifecare <akshit.sawhney@brightlifecare.com>
2016-09-30 14:32:34 +0530

    Filter functionality
    Filter functionality

--- public/scripts/keepApp.js
+++ public/scripts/keepApp.js
@@ -19,19 +19,22 @@ var existingData = [
         'key': 0,
         'label': 'OTHER',
         'title': 'REAL MADRID',
-        'text': 'HALA MADRID'
+        'text': 'HALA MADRID',
+        'display': true
     },
     {
         'key': 1,
         'label': 'FOOTBALL',
         'title': 'MAN UNITED',
-        'text': 'GLORY GLORY MAN UNITED'
+        'text': 'GLORY GLORY MAN UNITED',
+        'display': true
     },
     {
         'key': 2,
         'label': 'FOOTBALL',
         'title': 'LIVERPOOL',
-        'text': 'YOU WILL NEVER WALK ALONE'
+        'text': 'YOU WILL NEVER WALK ALONE',
+        'display': true
     }
 ];
 var OuterContainer = React.createClass({
@@ -67,11 +70,19 @@ var OuterContainer = React.createClass({
             keepData: newKeep
         })
     },
+    filterThis: function(filteredKeep) {
+        this.setState( {
+            labelValue: '',
+            titleValue: '',
+            noteValue: '',
+            keepData: filteredKeep
+        })
+    },
     render: function() {
         return (
             <div className="mainDiv">
                 <KeepInput keepNotePass={this.newKeep} keepNoteUpdate={this.updateKeep} keepNoteDiscard={this.discardThis} keep={this.state.keepData}/>
-                <KeepList keep={this.state.keepData}  titleValue={this.state.titleValue} noteValue={this.state.noteValue} />
+                <KeepList keep={this.state.keepData}  titleValue={this.state.titleValue} noteValue={this.state.noteValue} filterKeepData={this.filterThis} />
             </div>
         );
     }
@@ -100,6 +111,31 @@ var KeepList = React.createClass({
             var currentField = document.getElementsByClassName("text visibilityClass")[i].style.display="block";
         }
     },
+    filterFunction: function() {
+        var completeData = this.props.keep;
+        var filteredData = [];
+        var filterValue = document.getElementById("filterID").value;
+        if(filterValue == "No Label") {
+            for(var i=0; i< completeData.length; i++) {
+                completeData[i].display = true;
+            }
+            filteredData = completeData;
+        }
+        else {
+            for(var i=0; i< completeData.length; i++) {
+                if(completeData[i].label == filterValue) {
+                    completeData[i].display = true;
+                }
+                else {
+                    completeData[i].display = false;
+                }
+            }
+            filteredData = completeData;
+            console.log(filteredData);
+        }
+        this.props.filterKeepData(filteredData);
+
+    },
     render: function() {
         var self = this;
         var data = [];
@@ -120,21 +156,37 @@ var KeepList = React.createClass({
             else if(individualData.key % 5 == 4) {
                 classNameVar = "note green";
             }
-            data.push(
-                <div className={classNameVar} key={individualData.key} onDoubleClick={self.renderFromList.bind(self, individualData)}>
-                    <div className="title">
-                        {individualData.title}
-                        <span className="labelListClass" > {individualData.label}</span>
+            if(individualData.display) {
+                data.push(
+                    <div className={classNameVar} key={individualData.key} onDoubleClick={self.renderFromList.bind(self, individualData)}>
+                        <div className="title">
+                            {individualData.title}
+                            <span className="labelListClass" > {individualData.label}</span>
+                        </div>
+                        <p>
+                            {individualData.text}
+                        </p>
                     </div>
-                    <p>
-                        {individualData.text}
-                    </p>
-                </div>
-            );
-        })
+                );
+            }
+        });
+        var keepData = this.props.keep;
+        var labelArray = [];
+        keepData.forEach(function(individualData) {
+            labelArray.push(individualData.label);
+        });
+        var uniques = labelArray.unique();
+        var selectOptions = [];
+        selectOptions.push(<option key="blankKey" value="No Label">No Filtering</option>);
+        for(var i=0; i<uniques.length; i++) {
+            selectOptions.push(<option key={uniques[i]}> {uniques[i]} </option>);
+        };
         return (
             <div   onClick={this.blurFunction}>
                 {data}
+                <select onChange={this.filterFunction} defaultValue="No Label" id="filterID" className="labelListClass" ref="filterRef">
+                    {selectOptions}
+                </select>
             </div>
         )
     }
@@ -153,7 +205,8 @@ var KeepInput = React.createClass({
             var newObject = {
                 'label': labelValue,
                 'title': this.refs.titleField.value || 'No Title',
-                'text': this.refs.noteField.value
+                'text': this.refs.noteField.value,
+                'display': true
             };
             this.refs.titleField.value = '';
             this.refs.noteField.value = '';
@@ -190,7 +243,8 @@ var KeepInput = React.createClass({
             var newObject = {
                 'label': labelValue,
                 'title': this.refs.titleField.value || 'No Title',
-                'text': this.refs.noteField.value
+                'text': this.refs.noteField.value,
+                'display': true
             };
             this.refs.titleField.value = '';
             this.refs.noteField.value = '';
@@ -223,7 +277,6 @@ var KeepInput = React.createClass({
             this.refs.newLabel.value = '';
             var hiddenFields = document.getElementsByClassName("text visibilityClass");
             var oldKeep = this.props.keep;
-            console.log(oldKeep);
             for(var i=0; i< oldKeep.length; i++) {
                 if(oldKeep[i].key == keyValue) {
                     oldKeep.splice(i,1);
